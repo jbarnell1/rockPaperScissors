abcdf00faa03733a322d777a7fb2233ca9976f53
jbarnell <jonathanbarnell@gmail.com>
2022-01-20 14:15:33 -0600

    adding the full game functionality

--- scripts/rps_main.js
+++ scripts/rps_main.js
@@ -23,16 +23,19 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     if (playerSelection === computerSelection) {
-        return "You Tied! " + playerSelection + " ties with " + computerSelection;
+        console.log("You Tied! " + playerSelection + " ties with " + computerSelection);
+        return 0;
     } else if (playerSelection > computerSelection) {
         // the player has scissors and computer has paper (P wins) or rock (C wins)
         // OR 
         // the player has rock and computer has paper (C wins)
         // the only way for the player to win is if the computer has paper and player has scissors
         if (playerSelection === "scissors" && computerSelection === "paper") {
-            return "You Win! " + playerSelection + " beats " + computerSelection;
+            console.log("You Win! " + playerSelection + " beats " + computerSelection);
+            return 1;
         } else {
-            return "You Lose! " + computerSelection + " beats " + playerSelection;
+            console.log("You Lose! " + computerSelection + " beats " + playerSelection);
+            return -1;
         }
     } else {
         // the player has paper and the computer has scissors (C wins) or rock (P wins)
@@ -40,9 +43,25 @@ function playRound(playerSelection, computerSelection) {
         // the player has rock and the computer has scissors (P wins)
         // the only way for the computer to win is if the player has paper and computer has scissors
         if (computerSelection === "scissors" && playerSelection === "paper") {
-            return "You Lose! " + computerSelection + " beats " + playerSelection;
+            console.log("You Lose! " + computerSelection + " beats " + playerSelection);
+            return -1;
         } else {
-            return "You Win! " + playerSelection + " beats " + computerSelection;
+            console.log("You Win! " + playerSelection + " beats " + computerSelection);
+            return 1;
         }
+    }
+}
+
+function game() {
+    let running_total = 0;
+    for (let i=0; i < 5; i++) {
+        let playerSelection = prompt("What is your choice? Type 'rock', 'paper', or 'scissors'","rock");
+        let computerSelection = computerPlay();
+        running_total += playRound(playerSelection, computerSelection);
+    }
+    if (running_total > 0) {
+        alert("Great job, you beat the computer!!");
+    } else {
+        alert("Better luck next time...");
     }
 }
