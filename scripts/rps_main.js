6fc272af126b3f6f1e3c131e76c5e12451a5c3ef
jbarnell <jonathanbarnell@gmail.com>
2022-01-20 14:05:22 -0600

    added code to determine winner given player and computer inputs

--- scripts/rps_main.js
+++ scripts/rps_main.js
@@ -19,3 +19,30 @@ function computerPlay() {
     }
     return choice;
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection) {
+        return "You Tied! " + playerSelection + " ties with " + computerSelection;
+    } else if (playerSelection > computerSelection) {
+        // the player has scissors and computer has paper (P wins) or rock (C wins)
+        // OR 
+        // the player has rock and computer has paper (C wins)
+        // the only way for the player to win is if the computer has paper and player has scissors
+        if (playerSelection === "scissors" && computerSelection === "paper") {
+            return "You Win! " + playerSelection + " beats " + computerSelection;
+        } else {
+            return "You Lose! " + computerSelection + " beats " + playerSelection;
+        }
+    } else {
+        // the player has paper and the computer has scissors (C wins) or rock (P wins)
+        // OR
+        // the player has rock and the computer has scissors (P wins)
+        // the only way for the computer to win is if the player has paper and computer has scissors
+        if (computerSelection === "scissors" && playerSelection === "paper") {
+            return "You Lose! " + computerSelection + " beats " + playerSelection;
+        } else {
+            return "You Win! " + playerSelection + " beats " + computerSelection;
+        }
+    }
+}
